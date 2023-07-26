d75bf321b0fdceb594cb9dbe8548910c1112cc32
Dan Carlton <danlouis.carlton@gmail.com>
2023-07-26 05:55:34 -0700

    added header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F5385D',
+      }
+    },
   },
   plugins: [],
 }
