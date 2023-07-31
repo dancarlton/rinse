5f5355cac71bd029ac4ef3095fb44bdebee93837
Dan Carlton <danlouis.carlton@gmail.com>
2023-07-30 18:42:14 -0700

    organized files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import IndexPage from './pages/IndexPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import Layout from './Layout.jsx';
+import RegisterPage from './pages/RegisterPage';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:4000'
 
 function App() {
   return (
@@ -10,9 +14,11 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Route>
     </Routes>
   );
 }
 
 export default App;
+ 
