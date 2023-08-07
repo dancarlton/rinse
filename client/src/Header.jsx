7d30a7ee0e7d7754ad757d2dd350b4ed74dd42f5
Dan Carlton <danlouis.carlton@gmail.com>
2023-08-06 18:43:13 -0700

    organized files

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
@@ -9,17 +9,17 @@ export default function Header() {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke-width='1.5'
+          strokeWidth='1.5'
           stroke='currentColor'
-          class='w-6 h-6 -rotate-180 font-bold'>
+          className='w-6 h-6 -rotate-180 font-bold'>
           <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeLinecap='round'
+            strokeLinejoin='round'
             d='M15 10.5a3 3 0 11-6 0 3 3 0 016 0z'
           />
           <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeLinecap='round'
+            strokeLinejoin='round'
             d='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z'
           />
         </svg>
@@ -37,12 +37,12 @@ export default function Header() {
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
             viewBox='0 0 24 24'
-            stroke-width='1.5'
+            strokeWidth='1.5'
             stroke='currentColor'
-            class='w-6 h-6'>
+            className='w-6 h-6'>
             <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
+              strokeLinecap='round'
+              strokeLinejoin='round'
               d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
             />
           </svg>
@@ -56,9 +56,9 @@ export default function Header() {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke-width='1.5'
+          strokeWidth='1.5'
           stroke='currentColor'
-          class='w-6 h-6 relative top-1'>
+          className='w-6 h-6 relative top-1'>
           <path
             fillRule='evenodd'
             d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
@@ -69,11 +69,11 @@ export default function Header() {
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 24 24'
             fill='currentColor'
-            class='w-6 h-6 relative top-1'>
+            className='w-6 h-6 relative top-1'>
             <path
-              fill-rule='evenodd'
+              fillRule='evenodd'
               d='M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z'
-              clip-rule='evenodd'
+              clipRule='evenodd'
             />
           </svg>
         </div>
