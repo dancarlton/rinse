1106b7c09d66f912f8f930fad52e482b00da33a0
Dan Carlton <danlouis.carlton@gmail.com>
2023-08-16 14:31:01 -0700

    prior to converting to MVC app

--- api/models/User.js
+++ api/models/User.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: {type:String, unique: true},
-    password: String,
-})
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+});
 
-const UserModel = mongoose.model('User', UserSchema)
+const UserModel = mongoose.model('User', UserSchema);
 
-module.exports = UserModel
+module.exports = UserModel;
