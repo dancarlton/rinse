cf04c7f59ffa93f180e5ceae5b568c740db538e7
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-02 17:25:06 -0700

    updated README

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -3,6 +3,9 @@ import Header from '../Header.jsx';
 
 export default function IndexPage() {
   return (
-    <div>index page here</div>
+    <div className='text-4xl text-center mb-4 mt-4 items-center justify-around'>
+      <h1>Welcome to rinse!</h1>
+      <p>lets get that car cleaned</p>
+    </div>
   );
 }
