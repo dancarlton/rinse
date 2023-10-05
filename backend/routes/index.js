c85a0f8d935a9466f01c540711b83999ac198900
vincent <100250184+vguzman812@users.noreply.github.com>
2023-10-04 23:11:25 -0400

    Finish basic CRUD for backend user api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@ import errorHandler from "../middleware/errorMiddleware.js";
 
 export function initRoutes(app) {
   app.use("/api", baseRoutes);
-  app.use("/api/user", userRoutes);
+  app.use("/api/users", userRoutes);
   app.use("/api/auth", authRoutes);
   app.use("/healthcheck", (req, res) => res.send("Router routing OK"));
   app.use(errorHandler);
