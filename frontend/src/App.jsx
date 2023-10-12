ba419dfcc050cef507ce959d3a5ff5aec7beb0d8
vincent <100250184+vguzman812@users.noreply.github.com>
2023-10-12 16:26:35 -0400

    configrue eslint for front end.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,40 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import axios from "axios";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import axios from "axios"
 
 function App() {
-	// TODO: Make this a slice action in userSlice.js.
-	const [user, setUser] = useState(null);
-	// const hook = () => {
-	// 	console.log('effect')
-	// 	axios
-	// 		.get('http://localhost:5000/api/users')
-	// 		.then(response => {
-	// 			console.log('promise fulfilled')
-	// 			setUser(response)
-	// 		})
-	// }
-	// useEffect(hook, [])
-	// console.log(user)
+  // TODO: Make this a slice action in userSlice.js.
+  const [user, setUser] = useState(null);
+  // const hook = () => {
+  // 	console.log('effect')
+  // 	axios
+  // 		.get('http://localhost:5000/api/users')
+  // 		.then(response => {
+  // 			console.log('promise fulfilled')
+  // 			setUser(response)
+  // 		})
+  // }
+  // useEffect(hook, [])
+  // console.log(user)
 
-	return (
-		<BrowserRouter>
-			<div>
-				<Header />
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <LoginPage />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": "http://localhost:5000",
-		},
-	},
+  plugins: [eslint(), react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
+  },
 });
