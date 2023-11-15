acfdcfe002cda5b013f0d7f33ac1af6e39382dd9
vincent <100250184+vguzman812@users.noreply.github.com>
2023-11-15 18:53:46 -0500

    map working just fine for now. Eventually need to add the ability to enter a location and see all registered washers around you.

--- frontend/src/components/AutoComplete.jsx
+++ frontend/src/components/AutoComplete.jsx
@@ -3,6 +3,9 @@ import { setOrigin } from "../slices/navSlice";
 import { useDispatch } from "react-redux";
 import { useLazyGetPlaceDetailsQuery } from "../slices/navSlice";
 
+// https://developers.google.com/maps/documentation/places/web-service/autocomplete
+// I think initially we can get current users location and set that as the center of the map instead of initial state which is 0,0
+// this could be done in the navSlice, but maybe it would be better to do it here on initial render?
 const AutoComplete = () => {
   const dispatch = useDispatch();
   const [trigger, { data, error }] = useLazyGetPlaceDetailsQuery();
