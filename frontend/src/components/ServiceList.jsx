47ce235e1c573ce24f341a7fdf9bb3cefd7ae11d
Dan Carlton <danlouis.carlton@gmail.com>
2023-11-16 17:58:53 -0800

    implemented Card component

--- frontend/src/components/ServiceList.jsx
+++ frontend/src/components/ServiceList.jsx
@@ -1,43 +1,43 @@
 import React from 'react'
+import Card from './Card'
+import '../index.css'
+import sampleRinsers from '../data/sampleRinsers'
 
 const ServiceList = () => {
   return (
     <div className='services-list max-w-xl'>
-      {/* Choose a Wash */}
+      {/* --------Choose a Wash------- */}
       <h1 className='text-3xl lg:text-4xl font-bold'>Choose a Wash</h1>
-      {/* Recommended */}
+      {/* -------Recommended--------- */}
       <h2>Recommended</h2>
       <div className='carousel rounded-box'>
-        <div className='carousel-item'>
-          <img src='/images/providers/stock1.jpg' alt='Burger' />
-        </div>
-        <div className='carousel-item'>
-          <img src='/images/providers/stock2.jpg' alt='Burger' />
-        </div>
-        <div className='carousel-item'>
-          <img src='/images/providers/stock3.jpg' alt='Burger' />
-        </div>
-        <div className='carousel-item'>
-          <img src='/images/providers/stock4.jpg' alt='Burger' />
-        </div>
+        {sampleRinsers.map(rinser => (
+          <Card
+            key={rinser.id}
+            profileImage={rinser.profileImage}
+            altText={rinser.altText}
+            services={rinser.services[0]}
+            reviews={rinser.reviews}
+            serviceArea={rinser.serviceArea}
+          />
+        ))}
       </div>
-      {/* Best Deals */}
+      {/* <Card /> */}
+      {/* --------- Best Deals --------- */}
       <h2>Best Deals</h2>
       <div className='carousel rounded-box'>
-        <div className='carousel-item'>
-          <img src='/images/providers/stock4.jpg' alt='Burger' />
-        </div>
-        <div className='carousel-item'>
-          <img src='/images/providers/stock5.jpg' alt='Burger' />
-        </div>
-        <div className='carousel-item'>
-          <img src='/images/providers/stock6.jpg' alt='Burger' />
-        </div>
-        <div className='carousel-item'>
-          <img src='/images/providers/stock7.jpg' alt='Burger' />
-        </div>
+        {sampleRinsers.map(rinser => (
+          <Card
+            key={rinser.id}
+            profileImage={rinser.profileImage}
+            altText={rinser.altText}
+            services={rinser.services[0]}
+            reviews={rinser.reviews}
+            serviceArea={rinser.serviceArea}
+          />
+        ))}
       </div>
-      {/* Quickest */}
+      {/* -------- Quickest ---------- */}
       <h2>Quickest Service</h2>
       <div className='carousel rounded-box'>
         <div className='carousel-item'>
--- frontend/src/data/users.js
+++ frontend/src/data/users.js
@@ -2,111 +2,24 @@ import bcrypt from 'bcryptjs'
 
 const sampleUsers = [
   {
-    name: "admin",
-    email: "admin@email.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'admin',
+    email: 'admin@email.com',
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
-    name: "johnd",
-    email: "john@email.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'johnd',
+    email: 'john@email.com',
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: false,
   },
   {
-    name: "janed",
-    email: "jane@email.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: 'janed',
+    email: 'jane@email.com',
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: false,
   },
 ]
 
-const sampleRinsers = [
-  [
-    {
-      id: 1,
-      name: 'John Smith',
-      services: [
-        {
-          name: 'Exterior Wash',
-          price: 25.99,
-          estimatedTime: '1 hour',
-          rating: 4.5,
-        },
-        {
-          name: 'Interior Detailing',
-          price: 49.99,
-          estimatedTime: '2 hours',
-          rating: 4.8,
-        },
-        {
-          name: 'Waxing',
-          price: 34.99,
-          estimatedTime: '1.5 hours',
-          rating: 4.2,
-        },
-      ],
-      serviceArea: 'Downtown Area',
-      overallRating: 4.5,
-      reviews: [
-        { user: 'Jane Doe', rating: 4, comment: 'Great service!' },
-        { user: 'Bob Johnson', rating: 5, comment: 'Highly recommended.' },
-        {
-          user: 'Alice Williams',
-          rating: 4,
-          comment: 'Punctual and thorough.',
-        },
-      ],
-      travelDistance: 10, // in miles
-      locations: { latitude: 37.7749, longitude: -122.4194 },
-      profileImage: 'https://example.com/john-smith-profile.jpg',
-      coverPhoto: 'https://example.com/john-smith-cover.jpg',
-    },
-    {
-      id: 2,
-      name: 'Emily Davis',
-      services: [
-        {
-          name: 'Hand Wash',
-          price: 19.99,
-          estimatedTime: '45 minutes',
-          rating: 4.7,
-        },
-        {
-          name: 'Vacuuming',
-          price: 29.99,
-          estimatedTime: '1.5 hours',
-          rating: 4.6,
-        },
-        {
-          name: 'Leather Cleaning',
-          price: 39.99,
-          estimatedTime: '2 hours',
-          rating: 4.9,
-        },
-      ],
-      serviceArea: 'Suburban Heights',
-      overallRating: 4.7,
-      reviews: [
-        { user: 'David Brown', rating: 5, comment: 'Excellent work!' },
-        {
-          user: 'Sophia Rodriguez',
-          rating: 4,
-          comment: 'Convenient and efficient.',
-        },
-        {
-          user: 'Liam Thomas',
-          rating: 5,
-          comment: 'Always satisfied with their service.',
-        },
-      ],
-      travelDistance: 15, // in miles
-      locations: { latitude: 38.5816, longitude: -121.4944 },
-      profileImage: 'https://example.com/emily-davis-profile.jpg',
-      coverPhoto: 'https://example.com/emily-davis-cover.jpg',
-    },
-    // ... (similar entries for other service providers)
-  ],
-]
 
 export default users
