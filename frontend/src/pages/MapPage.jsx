d37d0e74708bd4da117fe7670c53e03ff2e3e51b
vincent <100250184+vguzman812@users.noreply.github.com>
2023-11-18 15:25:07 -0500

    conditonally render service list

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
-import AutoComplete from '../components/AutoComplete'
-import { useLoadScript } from '@react-google-maps/api'
+import React from "react";
+import AutoComplete from "../components/AutoComplete";
+import { useLoadScript } from "@react-google-maps/api";
 
-import Map from '../components/Map'
-import ServiceSearch from '../components/ServiceSearch'
-import ServiceList from '../components/ServiceList'
+import Map from "../components/Map";
+import ServiceSearch from "../components/ServiceSearch";
+import ServiceList from "../components/ServiceList";
+import { useSelector } from "react-redux";
 
 const MapPage = () => {
-  const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
-  const libraries = ['places']
+  const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  const libraries = ["places"];
   const { isLoaded } = useLoadScript({
     googleMapsApiKey,
     libraries,
-  })
+  });
+  // checking latitude, if it is 0 then we will not show service list
+  const latitude = useSelector((state) => state.nav.origin.location.latitude);
+
   return (
-    <div className='flex flex-col lg:flex-row mt-10 lg:space-x-4 lg:space-y-4'>
+    <div className="flex flex-col lg:flex-row mt-10 lg:space-x-4 lg:space-y-4">
       <ServiceSearch
-        style={{ minWidth: '100px', width: '150px' }}
-        className='min-w-service-search wider-service-search'
+        style={{ minWidth: "100px", width: "150px" }}
+        className="min-w-service-search wider-service-search"
       />
-      <ServiceList className='my-4 mx-2' />
+      {latitude !== 0 && <ServiceList className="my-4 mx-2" />}
       {isLoaded && (
-        <div className='lg:flex lg:w-[60%] space-y-4'>
+        <div className="lg:flex lg:w-[60%] space-y-4">
           {/* <div className="md:m-32 m-3 p-5 rounded max-h-64">
             <AutoComplete className="grid grow-2 h-full mx-32 my-32" />
           </div> */}
-          <div className='grid grow md:h-[89vh] lg:h-[80vh] lg:w-[60%] mx-3'>
+          <div className="grid grow md:h-[89vh] lg:h-[80vh] lg:w-[60%] mx-3">
             <Map />
           </div>
         </div>
       )}
     </div>
-  )
-}
-export default MapPage
+  );
+};
+export default MapPage;
