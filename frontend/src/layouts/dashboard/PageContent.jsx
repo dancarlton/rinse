c0edebee4253b03f53eec23e24b061c34ca48dc8
vincent <100250184+vguzman812@users.noreply.github.com>
2023-11-23 00:15:55 -0500

    add suspense content and error fallback

--- frontend/src/layouts/dashboard/PageContent.jsx
+++ frontend/src/layouts/dashboard/PageContent.jsx
@@ -2,8 +2,9 @@ import Header from "./Header";
 import { Route, Routes } from "react-router-dom";
 import routes from "../../routes/dashboardRoutes";
 import { useSelector } from "react-redux";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, Suspense } from "react";
 import ErrorPage from "../../pages/ErrorPage";
+import SuspenseContent from "../../components/SuspenseContent";
 
 function PageContent() {
   const mainContentRef = useRef(null);
@@ -23,19 +24,26 @@ function PageContent() {
       <main
         className="flex-1 overflow-y-auto pt-8 px-6  bg-base-200"
         ref={mainContentRef}>
-        <Routes>
-          {routes.map((route, key) => {
-            return (
-              <Route
-                key={key}
-                exact={true}
-                path={`${route.path}`}
-                element={<route.component />}
-                errorElement={<ErrorPage />}
-              />
-            );
-          })}
-        </Routes>
+        <Suspense fallback={<SuspenseContent />}>
+          <Routes>
+            {routes.map((route, key) => {
+              return (
+                <Route
+                  key={key}
+                  exact={true}
+                  path={`${route.path}`}
+                  element={<route.component />}
+                  errorElement={<ErrorPage />}
+                />
+              );
+            })}
+            {/* Redirecting unknown url to 404 page */}
+            <Route
+              path="*"
+              element={<ErrorPage />}
+            />
+          </Routes>
+        </Suspense>
         <div className="h-16"></div>
       </main>
     </div>
