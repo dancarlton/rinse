cd0ef16055f2a9908fa51439fbc62480c3d9c7b5
Andy-git985 <andy.yu617@gmail.com>
2024-01-08 21:13:20 -0500

    Added ProviderMarker component, markers displayed with userGetAllUsersQuery()

---
+++ frontend/src/components/Map/ProviderMarker.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { AdvancedMarker, InfoWindow } from '@vis.gl/react-google-maps';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCarSide } from '@fortawesome/free-solid-svg-icons';
+
+// Markers for provider positions
+const ProviderMarker = (props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <AdvancedMarker position={props.position}>
+        <FontAwesomeIcon icon={faCarSide} size='xl' />
+      </AdvancedMarker>
+      {open && (
+        <InfoWindow position={props.position} onCloseClick={() => setOpen(false)}>
+          <div>
+            {/* This button needs styling */}
+            <button onClick={handleClick}>Select this Provider</button>
+          </div>
+        </InfoWindow>
+      )}
+    </>
+  );
+};
+
+ProviderMarker.propTypes = {
+  position: PropTypes.shape({
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default ProviderMarker;
--- frontend/src/components/Map/index.jsx
+++ frontend/src/components/Map/index.jsx
@@ -1,13 +1,31 @@
 import { AdvancedMarker, APIProvider, Map as GoogleMap, Pin } from '@vis.gl/react-google-maps';
-
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import { useGetAllUsersQuery } from '../../slices/usersSlice';
+import ProviderMarker from './ProviderMarker';
 
 const Map = () => {
   const latitude = useSelector((state) => state.nav.origin.location.latitude);
   const longitude = useSelector((state) => state.nav.origin.location.longitude);
   const center = useMemo(() => ({ lat: latitude, lng: longitude }), [latitude, longitude]);
 
+  const { data: users, isLoading, isSuccess, isError } = useGetAllUsersQuery({ pageNumber: 1 });
+  let locations;
+  if (isLoading) {
+    return <p>Loading</p>;
+  } else if (isError) {
+    return <p>Error</p>;
+  } else if (isSuccess) {
+    locations = users
+      .filter((user) => user.locations && user.role === 'provider')
+      .map((provider) => {
+        const obj = {
+          lat: Number(provider.locations.latitude),
+          lng: Number(provider.locations.longitude),
+        };
+        return obj;
+      });
+  }
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       {/* get a map id here: https://developers.google.com/maps/documentation/get-map-id#create-a-map-id */}
@@ -15,6 +33,8 @@ const Map = () => {
         {/* Marker for user's current map position */}
         <AdvancedMarker position={center}>
           <Pin />
+          {locations !== undefined &&
+            locations.map((position) => <ProviderMarker key={position._id} position={position} />)}
         </AdvancedMarker>
       </GoogleMap>
     </APIProvider>
